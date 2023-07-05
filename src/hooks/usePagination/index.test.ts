d0d07e9ba75348c2b620138db49d9388cc148e3d
Madao <gn00678465@gmail.com>
2023-07-05 16:02:48 +0800

    test: 修正 unit test

--- src/hooks/usePagination/index.test.ts
+++ src/hooks/usePagination/index.test.ts
@@ -15,58 +15,58 @@ describe('usePagination', () => {
   describe('when page is 1', () => {
     it("returns the initial page number when prev() or next() haven't been called", () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
     });
 
     test('increments after calling next() when there are still pages left', () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].page).toBe(2);
+      expect(result.current.page).toBe(2);
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].page).toBe(3);
+      expect(result.current.page).toBe(3);
     });
 
     test("doesn't decrement after calling prev() when still on the first page", () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
       act(() => {
-        result.current[1].prev(1);
+        result.current.prev(1);
       });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
     });
 
     test("doesn't increment past the last page", () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].page).toBe(4);
+      expect(result.current.page).toBe(4);
     });
   });
 
   describe('when page is something other than 1', () => {
     test("returns the page number when prev() or next() haven't been called", () => {
       const { result } = setup({ defaultPage: 3, total: 40 });
-      expect(result.current[0].page).toBe(3);
+      expect(result.current.page).toBe(3);
     });
   });
 
   describe('when total is 0', () => {
     test('returns a page of 1', () => {
       const { result } = setup({ defaultPage: 1, total: 0 });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
     });
   });
 
@@ -74,25 +74,25 @@ describe('usePagination', () => {
     test('returns the maximum page number possible', () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
       act(() => {
-        result.current[1].setPage(1234);
+        result.current.setPage(1234);
       });
-      expect(result.current[0].page).toBe(4);
+      expect(result.current.page).toBe(4);
     });
 
     test('clamps the lower end of the range to 1', () => {
       const { result } = setup({ defaultPage: 1, total: 40 });
       act(() => {
-        result.current[1].setPage(1);
+        result.current.setPage(1);
       });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
       act(() => {
-        result.current[1].setPage(0);
+        result.current.setPage(0);
       });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
       act(() => {
-        result.current[1].setPage(-1234);
+        result.current.setPage(-1234);
       });
-      expect(result.current[0].page).toBe(1);
+      expect(result.current.page).toBe(1);
     });
   });
 
@@ -103,7 +103,7 @@ describe('usePagination', () => {
         total: 45,
         defaultPageSize: 14
       });
-      expect(result.current[0].pageSize).toBe(14);
+      expect(result.current.pageSize).toBe(14);
     });
 
     test('does not change pageSize when navigating through to the last page', () => {
@@ -114,13 +114,13 @@ describe('usePagination', () => {
       });
 
       act(() => {
-        result.current[1].prev(1);
+        result.current.prev(1);
       });
-      expect(result.current[0].pageSize).toBe(14);
+      expect(result.current.pageSize).toBe(14);
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].pageSize).toBe(14);
+      expect(result.current.pageSize).toBe(14);
     });
 
     test('when pageSize is not given, defaults to 10', () => {
@@ -128,7 +128,7 @@ describe('usePagination', () => {
         defaultPage: 1,
         total: 45
       });
-      expect(result.current[0].pageSize).toBe(10);
+      expect(result.current.pageSize).toBe(10);
     });
 
     test('更新 pageSize, pageCount 需一併更新', () => {
@@ -137,35 +137,35 @@ describe('usePagination', () => {
         total: 45
       });
       act(() => {
-        result.current[1].setPageSize(20);
+        result.current.setPageSize(20);
       });
-      expect(result.current[0].pageCount).toBe(3);
+      expect(result.current.pageCount).toBe(3);
     });
   });
 
   describe('isFirstPage', () => {
     test('returns true when on the first page', () => {
       const { result } = setup({ defaultPageSize: 20, total: 35 });
-      expect(result.current[0].isFirstPage).toBe(true);
+      expect(result.current.isFirstPage).toBe(true);
       act(() => {
-        result.current[1].prev(1);
+        result.current.prev(1);
       });
-      expect(result.current[0].isFirstPage).toBe(true);
+      expect(result.current.isFirstPage).toBe(true);
     });
     test('returns false when not the first page', () => {
       const { result } = setup({ defaultPageSize: 10, total: 35 });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].isFirstPage).toBe(false);
+      expect(result.current.isFirstPage).toBe(false);
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].isFirstPage).toBe(false);
+      expect(result.current.isFirstPage).toBe(false);
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].isFirstPage).toBe(false);
+      expect(result.current.isFirstPage).toBe(false);
     });
   });
 
@@ -173,21 +173,21 @@ describe('usePagination', () => {
     test('returns true when on the last page', () => {
       const { result } = setup({ defaultPageSize: 20, total: 35 });
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].isLastPage).toBe(true);
+      expect(result.current.isLastPage).toBe(true);
       act(() => {
-        result.current[1].next(1);
+        result.current.next(1);
       });
-      expect(result.current[0].isLastPage).toBe(true);
+      expect(result.current.isLastPage).toBe(true);
     });
     test('returns false when not the last page', () => {
       const { result } = setup({ defaultPageSize: 20, total: 35 });
-      expect(result.current[0].isLastPage).toBe(false);
+      expect(result.current.isLastPage).toBe(false);
       act(() => {
-        result.current[1].prev(1);
+        result.current.prev(1);
       });
-      expect(result.current[0].isLastPage).toBe(false);
+      expect(result.current.isLastPage).toBe(false);
     });
   });
 });
